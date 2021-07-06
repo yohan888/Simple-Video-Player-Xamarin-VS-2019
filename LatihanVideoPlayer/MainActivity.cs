ffca70ac2047083d53d9f58772b6ce3a9b163b3c
yohan888 <yohan888@users.noreply.github.com>
2021-07-06 17:01:06 +0700

    Simple Video Player Xamarin VS(2019)

---
+++ LatihanVideoPlayer/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Media;
+
+namespace LatihanVideoPlayer
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        VideoView videoView;
+        MediaController mediaController;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            videoView = (VideoView)FindViewById(Resource.Id.videoView1);
+            if (mediaController == null)
+            {
+                mediaController = new MediaController(this);
+                mediaController.SetAnchorView(videoView);
+            }
+            videoView.SetMediaController(mediaController);
+            MediaPlayer player = new MediaPlayer();
+            Android.Net.Uri uri = Android.Net.Uri.Parse("android.resource://" + PackageName + "/" + Resource.Raw.mood);
+            videoView.SetVideoURI(uri);
+            videoView.Start();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
